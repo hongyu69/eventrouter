088eddbb7706fdabdbaeb240b9178d3cc452515a
Jerry Sun <jesun@microsoft.com>
2026-01-09 15:11:35 +0800

    Update event id.

--- sinks/eventhub.go
+++ sinks/eventhub.go
@@ -127,7 +127,8 @@ func (h *EventHubSink) drainEvents(events []EventData) {
 
 	for i := 0; i < len(events); i++ {
 		eJSONBytes, err := json.Marshal(map[string]interface{}{
-			"Event": events[i].Event,
+			"id":    cosmicClusterId + "." + events[i].Event.Name,
+			"event": events[i].Event,
 		})
 		if err != nil {
 			glog.Warningf("Failed to flatten json: %v", err)
